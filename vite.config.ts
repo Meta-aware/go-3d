ebcecfc65fc2b9d888be85aaeb23fc597b0a23d8
Meta-aware <bkbecnel@gmail.com>
2026-09-13 20:54:29 -0500

    Set base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
+  base: "/go-3d/",
   plugins: [
     VitePWA({
       registerType: "autoUpdate",
